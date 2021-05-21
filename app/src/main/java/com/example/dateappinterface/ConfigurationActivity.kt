dd882cc9e8684886136b554bc7c665d4d814dff6
Radek <bielecki02@gmail.com>
2021-05-21 05:02:38 +0200

    added option to create an account and setting personal information -> all is saved to database

--- app/src/main/java/com/example/dateappinterface/ConfigurationActivity.kt
+++ app/src/main/java/com/example/dateappinterface/ConfigurationActivity.kt
@@ -3,16 +3,52 @@ package com.example.dateappinterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import java.lang.Exception
+import java.sql.Statement
 
 class ConfigurationActivity : AppCompatActivity() {
+    internal lateinit var name: EditText
+    internal lateinit var age: EditText
+    internal lateinit var description: EditText
+    internal lateinit var email: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
+        name = findViewById(R.id.name)
+        age = findViewById(R.id.age)
+        description = findViewById(R.id.description)
+        email = intent.getStringExtra("email").toString()
     }
 
     fun saveInfo(view: View){
-        //TODO("Save given information (NAME, AGE, description")
+        val nameString =name.text.toString()
+        val ageString =age.text.toString()
+        val descriptionString =description.text.toString()
+
+        var st: Statement
+        try{
+            var connectionHelper = ConnectionHelper()
+            var connect = connectionHelper.connectionclass()
+            if(connect != null){
+                var sql = "UPDATE tbl_UserInfo SET Name = '$nameString', Age = '$ageString', Description = '$descriptionString' WHERE Email = '$email'"
+                st = connect.createStatement()
+                st.execute(sql)
+                Log.d("msg","-----------------------------------------------SUCCCEESSSSSS-------------------------------")
+
+            }
+            else{
+                Log.d("sd","CONNECTION PROBLEM")
+            }
+
+        }
+        catch(ex: Exception){
+            var isSuccess = false
+            var z = ex.message.toString()
+            Log.d("error", z)
+        }
         val intent = Intent(this, PairSearchActivity::class.java).apply {  }
         startActivity(intent)
     }
--- app/src/main/java/com/example/dateappinterface/SigningActivity.kt
+++ app/src/main/java/com/example/dateappinterface/SigningActivity.kt
@@ -3,9 +3,13 @@ package com.example.dateappinterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import java.lang.Exception
+import java.sql.ResultSet
+import java.sql.Statement
 
 class SigningActivity : AppCompatActivity() {
     internal lateinit var email: EditText
@@ -15,10 +19,41 @@ class SigningActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signing)
         email = findViewById(R.id.editEmail)
         password = findViewById(R.id.editPassword)
+        Log.d("msg","-----------------------------------------------stworozno-------------------------------")
     }
 
     fun goToConfigurationScreen(view: View){
-        val intent = Intent(this, ConfigurationActivity::class.java).apply {  }
+        val name = ""
+        val age = 0
+        val description = ""
+        val mail = email.text.toString()
+        val pass = password.text.toString()
+        var st: Statement
+        try{
+            var connectionHelper = ConnectionHelper()
+            var connect = connectionHelper.connectionclass()
+            if(connect != null){
+                var sql = "INSERT INTO tbl_UserInfo (Email, Password, Name, Age, Description) VALUES ('$mail', '$pass', '$name', '$age', '$description')"
+                st = connect.createStatement()
+                st.execute(sql)
+                Log.d("msg","-----------------------------------------------SUCCCEESSSSSS-------------------------------")
+
+            }
+            else{
+                Log.d("sd","CONNECTION PROBLEM")
+            }
+
+        }
+        catch(ex: Exception){
+            var isSuccess = false
+            var z = ex.message.toString()
+            Log.d("error", z)
+        }
+        val intent = Intent(this, ConfigurationActivity::class.java).apply {
+            putExtra("email",email.text.toString())
+        }
         startActivity(intent)
     }
+
+
 }
